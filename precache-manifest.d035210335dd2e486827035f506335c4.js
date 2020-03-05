ba66ebb808d9fca0e63c6794d15310267445b3ea
Raden Ramadista <raden.ramadista@gmail.com>
2020-03-05 19:38:27 +0700

    Updates

---
+++ precache-manifest.d035210335dd2e486827035f506335c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b536d4bc5b2fdec3ff869bce8a7e17",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "84e148212d1a51c4e7a3",
+    "url": "/learn-react/static/css/main.cf2eacaf.chunk.css"
+  },
+  {
+    "revision": "a8ecd558a20727e44dc8",
+    "url": "/learn-react/static/js/2.3a22d220.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/learn-react/static/js/2.3a22d220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e148212d1a51c4e7a3",
+    "url": "/learn-react/static/js/main.87a6049f.chunk.js"
+  },
+  {
+    "revision": "1b71aed738c35da99381",
+    "url": "/learn-react/static/js/runtime-main.824098c2.js"
+  }
+]);
